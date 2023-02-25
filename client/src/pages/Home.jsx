58fb8dd9465108a342344364291214171c256bf9
AbrahamDN <abe.ndinga@gmail.com>
2023-02-25 14:13:36 +0000

    refactor: Card - style

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Card, FormField, Loader } from "../components";
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0)
-    return data.map((post, idx) => <Card key={`card-${idx}`} {...post} />);
+    return data.map((post) => <Card key={post._id} {...post} />);
   return (
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
   );
